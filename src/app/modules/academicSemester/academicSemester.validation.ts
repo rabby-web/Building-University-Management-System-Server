031f4fd28e8c9efaf667646d7a1b28e1f7bfdeec
rabby-web <rabby.webdeveloper@gmail.com>
2024-06-04 09:37:27 +0600

    createAcademicSemesterValidationSchema complete

--- src/app/modules/academicSemester/academicSemester.validation.ts
+++ src/app/modules/academicSemester/academicSemester.validation.ts
@@ -1,8 +1,18 @@
 import { z } from 'zod';
+import {
+  AcademicSemesterCode,
+  AcademicSemesterName,
+  Months,
+} from './academicSemester.constants';
+import { TAcademicSemesterName } from './academicSemester.interface';
 
 const createAcademicSemesterValidationSchema = z.object({
   body: z.object({
-    name: z.enum(['Autumn', 'Summer', 'Fall']),
+    name: z.enum([...AcademicSemesterName] as [string, ...string[]]),
+    code: z.enum([...AcademicSemesterCode] as [string, ...string[]]),
+    year: z.date(),
+    startMonth: z.enum([...Months] as [string, ...string[]]),
+    endMonth: z.enum([...Months] as [string, ...string[]]),
   }),
 });
 

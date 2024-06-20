4aad811a79cf20ce085b81e35d4724c24c42d960
rabby-web <rabby.webdeveloper@gmail.com>
2024-06-20 15:31:45 +0600

    create student by year code and semester

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -13,14 +13,30 @@ const findLastStudentId = async () => {
   )
     .sort({ createAt: -1 })
     .lean();
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 // year semesterCode 4 digit number
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId =
-    (await findLastStudentId()) || (0).toString().padStart(4, '0');
+  // first time  0000 // 0001 => 1
+  let currentId = (0).toString(); // 0000 by default
+
+  const lastStudentId = await findLastStudentId();
+  // 2030 01 0001
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6); //01;
+  const lastStudentYear = lastStudentId?.substring(0,4); //2030
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+
+
+  if (lastStudentId && lastStudentSemesterCode === currentSemesterCode  && lastStudentYear === currentYear) {
+    currentId = lastStudentId.substring(6) // 0001
+
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+
   incrementId = `${payload.year}${payload.code}${incrementId}`;
+
   return incrementId;
 };

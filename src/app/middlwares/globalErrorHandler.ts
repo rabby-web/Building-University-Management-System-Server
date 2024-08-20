f07418b0f0bb5941e52287e19bf3ab0c6046ee72
rabby-web <rabby.webdeveloper@gmail.com>
2024-08-20 20:06:45 +0600

    find zod error with zod subclass

--- src/app/middlwares/globalErrorHandler.ts
+++ src/app/middlwares/globalErrorHandler.ts
@@ -1,19 +1,38 @@
+import { path } from 'path';
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Something went wrong!';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  // setting default values
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong!';
+
+  type TErrorSource = {
+    path: string | number;
+    message: string;
+    // error: any;
+  }[];
+  let errorSource: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong',
+      // error: err,
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    statusCode = 400;
+    message = 'zod error';
+  }
+
   return res.status(statusCode).json({
+    success: false,
     message,
+    errorSource,
     error: err,
   });
 };

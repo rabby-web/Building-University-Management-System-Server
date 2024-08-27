497ebbcd7e426283bca71b7ee624a0e89c5b5d5b
rabby-web <rabby.webdeveloper@gmail.com>
2024-08-27 21:44:13 +0600

    function handles errors thrown by Zod

--- src/app/errors/handleZodError.ts
+++ src/app/errors/handleZodError.ts
@@ -1,7 +1,8 @@
 import { ZodError, ZodIssue } from 'zod';
 import { TErrorSources, TGenericErrorResponse } from '../interface/error';
 
-const handleZodError = (err: ZodError):TGenericErrorResponse => {
+// This function handles errors thrown by Zod, a validation library, and formats them into a standardized error response
+const handleZodError = (err: ZodError): TGenericErrorResponse => {
   const errorSources: TErrorSources = err.issues.map((issue: ZodIssue) => {
     return {
       path: issue?.path[issue.path.length - 1],
